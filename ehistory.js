5a3b1926d211052aa3fdec8717bcd15e0c6f5637
Amjad Masad <amjad.masad@gmail.com>
2011-03-08 03:25:43 +0200

    cleanup

--- ehistory.js
+++ ehistory.js
@@ -3,311 +3,316 @@ function EHistory(){
 	this.count=0;
 }
 
-_ = jQuery;
+
 	
 //Ehistory - Public: -------------------------------------------------------------
 /**
  *public do
  *
 */
+(function ($){
+	EHistory.prototype.doSearch = function(query, options) {//@FIXME make one argument
+		this.settings = {
+			'text': '',
+			'startTime' : 0,
+			'endTime' : (new Date).getTime(),
+			'maxResults' : Infinity,
+			'regex': 0
+		};
 
-EHistory.prototype.doSearch = function(query, options) {//@FIXME make one argument
-	this.settings = {
-		'text': '',
-		'startTime' : 0,
-		'endTime' : (new Date).getTime(),
-		'maxResults' : 9999999,
-		'regex': 0
-	};
-	
-	var options = {};
-	this.searchText_ = query;
-	this.filters_ = [];
-	this.resultsByDate = {};
-	
-	var s = takeAndReturn(this.settings, this.searchText_, (function(self){
-		return function(place, key, value){
-			if (key == 'regex'){
-				self.settings[key] = value;
-				return;
-			}
-			self.settings[key]=(new Date(value)).getTime();
+		var options = {};
+		this.searchText_ = query;
+		this.filters_ = [];
+		this.resultsByDate = {};
+
+		var s = takeAndReturn(this.settings, this.searchText_, (function(self){
+			return function(place, key, value){
+				if (key == 'regex'){
+					self.settings[key] = value;
+					return;
+				}
+				self.settings[key]=(new Date(value)).getTime();
 			}
 		})(this));
-		
-	this.strippedSearchText_ = this.initiateFilters_(s);
-	
-	_.extend(this.settings, {text: this.strippedSearchText_});
-	if(options)
-		_.extend(this.settings, options);
 
-	var self = this;
-	var searchSettings = {};
-	_.extend(searchSettings, this.settings);
-	//delete settings unrelavent to chrome history api
-	delete searchSettings['regex'];
-	function test(results){
-		self.processResults_(results)
-	}
-	chrome.history.search(searchSettings,test);
-}
+		this.strippedSearchText_ = this.initiateFilters_(s);
 
-EHistory.prototype.removeURLsOnOneDay = function (obj){
-	for(var i = 0; i<obj.length; i+=2){
-		day = new Date(new Date(obj[i] * 1000).toDateString()).getTime();	
-		nextDay = day + 24 * 60 * 60 * 1000;
-		console.log(day);
+		$.extend(this.settings, {text: this.strippedSearchText_});
+		if(options)
+			$.extend(this.settings, options);
+
 		var self = this;
-		for (var j = 0; j < obj[i+1].length; j++){
-			chrome.history.getVisits({url:obj[i+1][j]}, function(visits){self.filterAndRemove_(visits,day,nextDay)});
+		var searchSettings = {};
+		$.extend(searchSettings, this.settings);
+		//delete settings unrelavent to chrome history api
+		delete searchSettings['regex'];
+		function test(results){
+			self.processResults_(results)
 		}
-		
+		chrome.history.search(searchSettings,test);
 	}
-		
-	console.log(obj);
-	}
-//public static:
-EHistory.loadInLocalStorage = function(){
-	var getVisits = function(results){
-		for(var i=0; i < results.length; i++)
-		{
-			chrome.history.getVisits({url:results[i].url}, (function(res){return function(vis){storeVisit(vis, res);}})(results[i]));
-		}
-	};
-	var storeVisit = function(visits, hitem){
-		for (var i=0; i < visits.length; i++){
-			_.extend(visits[i], hitem);
-			localStorage[visits[i].visitId] = JSON.stringify(visits[i]); 
-		}
-		
-	};
-	chrome.history.search({text:'', startTime:0, endTime: (new Date).getTime(), maxResults:99999999}, getVisits);
-}
-	//chrome.history.deleteRange({startTime: time,
-								//	endTime: time+0.1}, deleteComplete);
 
-//Ehistory - Private: -------------------------------------------------------------
-/**
- *initiate filters_ from search query with filter functions 
- *@param {String} q Search Text from the browser history form
- *@returl {String} qString returns plain search text without the operators
- */
-EHistory.prototype.initiateFilters_ = function(q){
-	var qString = "";
-	var reg = /^\w+:\S+$/;
-	var words = q.split(' ');
-	for (var i=0; i < words.length; i++){
-		var oneWord = words[i];
-		
-		if (reg.test(oneWord)) {
-			var method = oneWord.split(":")[0];
-			if (Filters[method]){
-				this.filters_.push({'func' : Filters[method], 'text' : oneWord.split(":")[1] });
-				continue;
+	EHistory.prototype.removeURLsOnOneDay = function (obj){
+		for(var i = 0; i<obj.length; i+=2){
+			day = new Date(new Date(obj[i] * 1000).toDateString()).getTime();	
+			nextDay = day + 24 * 60 * 60 * 1000;
+			var self = this;
+			for (var j = 0; j < obj[i+1].length; j++){
+				chrome.history.getVisits({url:obj[i+1][j]}, function(visits){self.filterAndRemove_(visits,day,nextDay)});
 			}
-		}
-		qString+= " " + oneWord;
-	}
-	return trim(qString);
-}
-/**
- * process the results return from the history api and call functions to filter and sendback to browser
- *@param {HistoryItem[]} results return from history api
- *
- */
-EHistory.prototype.processResults_ = function(results){
-//console.log(results);
-	results = this.filterOutResults_(results);
-	this.results_ = {};
-	var self = this;
-	for (var i = 0; i < results.length; i++){
-		this.results_.length_ = i+1;
-		this.results_[results[i].id] = results[i];
-		chrome.history.getVisits({url : results[i].url}, function(visits){self.fixAndSendBack_(visits);});
-	}
-}
 
-EHistory.prototype.filterOutResults_ = function(results){
-	var results__ = [];
-	for (var i=0; i < results.length; i++)
-	{
-		var add = true;
-		for (var j = 0; j < this.filters_.length; j++)
-		{
-			_.extend(results[i], this.settings);
-			methodObject = this.filters_[j];
-			if (!methodObject.func(results[i], methodObject.text))
-				{
-					add = false;
-					break;
+		}
+		}
+	//public static:
+	EHistory.loadInLocalStorage = function(){
+		var getVisits = function(results){
+			for(var i=0; i < results.length; i++)
+			{
+				chrome.history.getVisits({url:results[i].url}, (function(res){return function(vis){storeVisit(vis, res);}})(results[i]));
+			}
+		};
+		var storeVisit = function(visits, hitem){
+			for (var i=0; i < visits.length; i++){
+				$.extend(visits[i], hitem);
+				localStorage[visits[i].visitId] = JSON.stringify(visits[i]); 
+			}
+
+		};
+		chrome.history.search({
+			text:'',
+			startTime:0, 
+			endTime: (new Date).getTime(),
+			maxResults:Infinity
+			}, getVisits);
+	}
+		//chrome.history.deleteRange({startTime: time,
+									//	endTime: time+0.1}, deleteComplete);
+
+	//Ehistory - Private: -------------------------------------------------------------
+	/**
+	 *initiate filters_ from search query with filter functions 
+	 *@param {String} q Search Text from the browser history form
+	 *@returl {String} qString returns plain search text without the operators
+	 */
+	EHistory.prototype.initiateFilters_ = function(q){
+		var qString = "";
+		var reg = /^\w+:\S+$/;
+		var words = q.split(' ');
+		for (var i=0; i < words.length; i++){
+			var oneWord = words[i];
+
+			if (reg.test(oneWord)) {
+				var method = oneWord.split(":")[0];
+				if (Filters[method]){
+					this.filters_.push({'func' : Filters[method], 'text' : oneWord.split(":")[1] });
+					continue;
 				}
-		}
-		if (add)
-			results__.push(results[i]);
-	}
-	return results__;
-}
-
-EHistory.prototype.fixAndSendBack_ = function(visits){
-	
-	var historyItem = this.results_[visits[0].id];
-	delete this.results_[visits[0].id];
-	this.results_.length_--;
-	if (!historyItem) return;
-	
-    if (historyItem.title == "") 
-		historyItem.title = historyItem.url;
-		
-	for(var i = 0; i < visits.length; i++){
-		this.count++;
-		var visitItem = visits[i];
-		visitItem.time = visitItem.visitTime /1000;
-		visitItem.url = historyItem.url;
-		visitItem.title = historyItem.title;
-		var now = new Date();
-		var date = new Date(visitItem.visitTime);
-		var dif = now.getTime() - date.getTime();
-		var difInDays = Math.floor(dif / 1000 / 60 / 60 / 24);
-		var timeStr = date.getHours() % 12;
-		var dateStr = "";
-
-		if (timeStr.length < 2)
-			timeStr = "0" + timeStr;
-
-		timeStr += ":" + date.getMinutes(); 
-		timeStr += date.getHours() > 11 ? " P.M" : " A.M";
-		//if (difInDays == 0) dateStr += "Today - ";
-		//if (difInDays == 1) dateStr += "Yesterday - ";
-		dateStr += date.toDateString();
-		
-		visitItem.dateRelativeDay = dateStr;
-		visitItem.dateTimeOfDay = timeStr;
-		visitItem.dateShort = date.toDateString();
-		if (!this.resultsByDate[visitItem.dateRelativeDay])
-			this.resultsByDate[visitItem.dateRelativeDay] = {};
-
-		this.resultsByDate[visitItem.dateRelativeDay][historyItem.id] = visitItem;
-	//	historyResult({'term': this.searchText_, 'finished': true}, [visitItem]);
-	}
-	
-	if (this.results_.length_ == 0)
-		{
-			this.sendBack_();
-		}
-	
-	
-	var finished = this.results_.length_ == 0 ? true : false;
-
-}
-
-EHistory.prototype.sendBack_ = function(){
-	var finalResults = [];
-	for (var i in this.resultsByDate){
-		
-		for(var j in this.resultsByDate[i])
-		{
-			//filter out out of date range visits
-			if (this.resultsByDate[i][j].visitTime > this.settings.endTime || this.resultsByDate[i][j].visitTime < this.settings.startTime) continue;
-			finalResults.push(this.resultsByDate[i][j]);
-		}
-	}
-	finalResults.sort(function (a,b){
-		if (a.visitTime > b.visitTime)
-			return -1;
-		return 1;
-	});
-	this.results_ = finalResults;
-	console.log(this.count);
-	historyResult({'term': this.searchText_, 'finished': true}, finalResults);
-}
-
-EHistory.prototype.filterAndRemove_ = function(visits, day, next){
-	console.log(next);		
-	for (var i = 0; i < visits.length; i++){
-		var time = visits[i].visitTime;
-		if (time > day && time < next){
-			chrome.history.deleteRange({
-				startTime: time-0.1,
-				endTime: time+ 0.1
-			}, function(){console.log(time)});
-		}
-	}
-	deleteComplete();
-}
-function trim(str){
-	return str.replace(/^\s+|\s+$/g,'');
-}
-
-function parseUrl(url){
-	//@todo implement protocol parsing
-      //@todo use regex
-      //@todo implement port parsing
-      url = url.replace("http://", "");
-      //console.log(url.slice("/"));
-      hostName = url.split("/")[0];
-      path = url.replace(hostName+"/", "");
-      return {'hostName' : hostName, 'path' : path};	
-}
-function takeAndReturn(pairs, query, callback){
-	var words = query.split(' ');
-	
-	var rString = "";
-	for (var i=0; i < words.length; i++){
-		    var oneWord = words[i];		
-			var key = oneWord.split(":")[0];
-			var value = oneWord.split(":")[1] || "";
-			if (typeof pairs[key] !== "undefined"){
-				callback(pairs[key], key, value);
-				continue;
 			}
-		rString+= " " + oneWord;
+			qString+= " " + oneWord;
+		}
+		return trim(qString);
 	}
-	return rString;
-}
+	/**
+	 * process the results return from the history api and call functions to filter and sendback to browser
+	 *@param {HistoryItem[]} results return from history api
+	 *
+	 */
+	EHistory.prototype.processResults_ = function(results){
+		results = this.filterOutResults_(results);
+		this.results_ = {};
+		var self = this;
+		for (var i = 0; i < results.length; i++){
+			this.results_.length_ = i+1;
+			this.results_[results[i].id] = results[i];
+			chrome.history.getVisits({ url: results[i].url },function(visits){
+					self.fixAndSendBack_(visits);
+			});
+		}
+	}
 
-function isValidRegex(regex) {
-  try {
-    new RegExp(regex);
-  } catch (e) {
-    return false;
-  }
-  return true;
-}
-var Filters = {
-	//@todo implement validation on returned from parseUrl
-	   //@todo think about upper/lower case
-		'intitle': function(obj, txtMatch){
-			var isRegex = obj.regex && obj.regex == "1" && isValidRegex(txtMatch);
-			if (isRegex){
-				return (new RegExp(txtMatch)).test(obj.title);
-			}else{
-				return (obj.title.toLowerCase().indexOf(txtMatch.toLowerCase()) > -1);
-			}	
-		},
-	      'inurl': function(obj, txtMatch){
-	          	var isRegex = obj.regex && obj.regex == "1" && isValidRegex(txtMatch);
+	EHistory.prototype.filterOutResults_ = function(results){
+		var results__ = [];
+		for (var i=0; i < results.length; i++)
+		{
+			var add = true;
+			for (var j = 0; j < this.filters_.length; j++)
+			{
+				$.extend(results[i], this.settings);
+				methodObject = this.filters_[j];
+				if (!methodObject.func(results[i], methodObject.text))
+					{
+						add = false;
+						break;
+					}
+			}
+			if (add)
+				results__.push(results[i]);
+		}
+		return results__;
+	}
+
+	EHistory.prototype.fixAndSendBack_ = function(visits){
+
+		var historyItem = this.results_[visits[0].id];
+		delete this.results_[visits[0].id];
+		this.results_.length_--;
+		if (!historyItem) return;
+
+	    if (historyItem.title == "") 
+			historyItem.title = historyItem.url;
+
+		for(var i = 0; i < visits.length; i++){
+			this.count++;
+			var visitItem = visits[i];
+			visitItem.time = visitItem.visitTime /1000;
+			visitItem.url = historyItem.url;
+			visitItem.title = historyItem.title;
+			var now = new Date();
+			var date = new Date(visitItem.visitTime);
+			var dif = now.getTime() - date.getTime();
+			var difInDays = Math.floor(dif / 1000 / 60 / 60 / 24);
+			var timeStr = date.getHours() % 12;
+			var dateStr = "";
+
+			if (timeStr.length < 2)
+				timeStr = "0" + timeStr;
+
+			timeStr += ":" + date.getMinutes(); 
+			timeStr += date.getHours() > 11 ? " P.M" : " A.M";
+			//if (difInDays == 0) dateStr += "Today - ";
+			//if (difInDays == 1) dateStr += "Yesterday - ";
+			dateStr += date.toDateString();
+
+			visitItem.dateRelativeDay = dateStr;
+			visitItem.dateTimeOfDay = timeStr;
+			visitItem.dateShort = date.toDateString();
+			if (!this.resultsByDate[visitItem.dateRelativeDay])
+				this.resultsByDate[visitItem.dateRelativeDay] = {};
+
+			this.resultsByDate[visitItem.dateRelativeDay][historyItem.id] = visitItem;
+		//	historyResult({'term': this.searchText_, 'finished': true}, [visitItem]);
+		}
+
+		if (this.results_.length_ == 0)
+			{
+				this.sendBack_();
+			}
+
+
+		var finished = this.results_.length_ == 0 ? true : false;
+
+	}
+
+	EHistory.prototype.sendBack_ = function(){
+		var finalResults = [];
+		for (var i in this.resultsByDate){
+
+			for(var j in this.resultsByDate[i])
+			{
+				//filter out out of date range visits
+				if (this.resultsByDate[i][j].visitTime > this.settings.endTime || this.resultsByDate[i][j].visitTime < this.settings.startTime) continue;
+				finalResults.push(this.resultsByDate[i][j]);
+			}
+		}
+		finalResults.sort(function (a,b){
+			if (a.visitTime > b.visitTime)
+				return -1;
+			return 1;
+		});
+		this.results_ = finalResults;
+		console.log(this.count);
+		historyResult({'term': this.searchText_, 'finished': true}, finalResults);
+	}
+
+	EHistory.prototype.filterAndRemove_ = function(visits, day, next){
+		for (var i = 0; i < visits.length; i++){
+			var time = visits[i].visitTime;
+			if (time > day && time < next){
+				//a hack to delete one item, unless one can visit multiple urls in the same .1 milliseconds
+				//chrome history system does not know any less.
+				chrome.history.deleteRange({
+					startTime: time-0.1,
+					endTime: time+ 0.1
+				}, $.noop);
+			}
+		}
+		deleteComplete();
+	}
+	function trim(str){
+		return str.replace(/^\s+|\s+$/g,'');
+	}
+
+	function parseUrl(url){
+		//@todo implement protocol parsing
+	      //@todo use regex
+	      //@todo implement port parsing
+	      url = url.replace("http://", "");
+	      //console.log(url.slice("/"));
+	      hostName = url.split("/")[0];
+	      path = url.replace(hostName+"/", "");
+	      return {'hostName' : hostName, 'path' : path};	
+	}
+	function takeAndReturn(pairs, query, callback){
+		var words = query.split(' ');
+
+		var rString = "";
+		for (var i=0; i < words.length; i++){
+			    var oneWord = words[i];		
+				var key = oneWord.split(":")[0];
+				var value = oneWord.split(":")[1] || "";
+				if (typeof pairs[key] !== "undefined"){
+					callback(pairs[key], key, value);
+					continue;
+				}
+			rString+= " " + oneWord;
+		}
+		return rString;
+	}
+
+	function isValidRegex(regex) {
+	  try {
+	    new RegExp(regex);
+	  } catch (e) {
+	    return false;
+	  }
+	  return true;
+	}
+	var Filters = {
+		//@todo implement validation on returned from parseUrl
+		   //@todo think about upper/lower case
+			'intitle': function(obj, txtMatch){
+				var isRegex = obj.regex && obj.regex == "1" && isValidRegex(txtMatch);
 				if (isRegex){
-					return (new RegExp(txtMatch)).test(obj.url);
+					return (new RegExp(txtMatch)).test(obj.title);
 				}else{
-					return (obj.url.toLowerCase().indexOf(txtMatch.toLowerCase()) > -1);
-				}
-	      },
-	      'site' : function(obj, host){
-	          var hostName = parseUrl(obj.url).hostName.split(".");
-	          host = host.split(".");
-	          //handle stuff like site:.jo or ..jo
-	          for (var x in host)
-	            if (host[x] == "") host.splice(x,1);
-				
-	          //j is where to start comparing in the hostname of the url in question
-	          var j = hostName.length - host.length;
-	          for(var i in host)
-	          {
-	              //if j is undefined or doesn't equal the hostname to match return false 
-	              if (!hostName[j] || hostName[j].toLowerCase() != host[i].toLowerCase())
-	                  return false;
-	              j++;
-	          }
-	          return true;
-	      }
-};
+					return (obj.title.toLowerCase().indexOf(txtMatch.toLowerCase()) > -1);
+				}	
+			},
+		      'inurl': function(obj, txtMatch){
+		          	var isRegex = obj.regex && obj.regex == "1" && isValidRegex(txtMatch);
+					if (isRegex){
+						return (new RegExp(txtMatch)).test(obj.url);
+					}else{
+						return (obj.url.toLowerCase().indexOf(txtMatch.toLowerCase()) > -1);
+					}
+		      },
+		      'site' : function(obj, host){
+		          var hostName = parseUrl(obj.url).hostName.split(".");
+		          host = host.split(".");
+		          //handle stuff like site:.jo or ..jo
+		          for (var x in host)
+		            if (host[x] == "") host.splice(x,1);
+
+		          //j is where to start comparing in the hostname of the url in question
+		          var j = hostName.length - host.length;
+		          for(var i in host)
+		          {
+		              //if j is undefined or doesn't equal the hostname to match return false 
+		              if (!hostName[j] || hostName[j].toLowerCase() != host[i].toLowerCase())
+		                  return false;
+		              j++;
+		          }
+		          return true;
+		      }
+	};
+})( jQuery );
--- init.js
+++ init.js
@@ -1,4 +1,4 @@
-(function ($){_=$;
+(function ($){
 	//inject search options into page
 	var parseQuery = function(inputs){
 		var operators = "";
@@ -15,10 +15,10 @@
 			if (key == "regex"){
 				place.attr('checked', value == "1" ? true:false);
 			}
-			place.val(_.trim(place.val() + ' ' + value));
+			place.val($.trim(place.val() + ' ' + value));
 		});		
 		
-		if (inputs['words']) inputs['words'].val(_.trim(words));
+		if (inputs['words']) inputs['words'].val($.trim(words));
 	};
 	
 	var parseSettings = function(inputs){
@@ -36,32 +36,32 @@
 				inputs['searchBox'].val(inputs['searchBox'].val() + ' ' + j + ':'+filters[i]);
 			}
 		}
-		inputs['searchBox'].val(_.trim(inputs['searchBox'].val()));
+		inputs['searchBox'].val($.trim(inputs['searchBox'].val()));
 	};
 	
-	_(document).ready(function (){
-		//_('<table id="res"><tbody><tr><td></td><td><h3>How did I get here?</h3><table id="how"></table></td></tr></tbody></table>').appendTo(_('.main'));
-		//_('#res tbody td:first').append(_('#results-display'));
-		var optionsPage = _('<div>').append(_.ajax({url: 'advanced.html',async:false}).responseText);
-		var header = _('.header');
+	$(document).ready(function (){
+		//$('<table id="res"><tbody><tr><td></td><td><h3>How did I get here?</h3><table id="how"></table></td></tr></tbody></table>').appendTo($('.main'));
+		//$('#res tbody td:first').append($('#results-display'));
+		var optionsPage = $('<div>').append($.ajax({url: 'advanced.html',async:false}).responseText);
+		var header = $('.header');
 		header.append(optionsPage.find('#show-options')).append('Advanced options');
 		header.append(optionsPage.find("#options-container"));
 		
-		var showButton = _('#show-options');
-		var searchBox = _('#term');
-		var options = _('#options-container');
-		var submit = _('[name=submit]');
-		var advanced_search = _('#advanced-search');
+		var showButton = $('#show-options');
+		var searchBox = $('#term');
+		var options = $('#options-container');
+		var submit = $('[name=submit]');
+		var advanced_search = $('#advanced-search');
 		//initial bindings
 		var inputs = {
-			inurl : _('#url_text input'),
-			intitle: _('#title_text input'),
-			site: _('#site_text input'),
+			inurl : $('#url_text input'),
+			intitle: $('#title_text input'),
+			site: $('#site_text input'),
 			'searchBox': searchBox,
-			words: _('#all-words_text input'),
-			startTime: _('#from-date_text input'),
-			endTime: _('#to-date_text input'),
-			regex: _('#regex_cb input')
+			words: $('#all-words_text input'),
+			startTime: $('#from-date_text input'),
+			endTime: $('#to-date_text input'),
+			regex: $('#regex_cb input')
 		};
 		//listen for checkbox change to set checked or unchecked
 		inputs['regex'].change(function(e){
@@ -73,43 +73,43 @@
 			console.log(e);
 		});
 		showButton.click(function(){
-			if (_('form:visible').length > 0){
+			if ($('form:visible').length > 0){
 				parseQuery(inputs);
 			}else{
 				parseSettings(inputs);
 			}
-			_('form').toggle();
+			$('form').toggle();
 			options.toggle();	
 		});
 		
 		advanced_search.click(function(){
 			parseSettings(inputs);
 			var options = {};
-			RESULTS_PER_PAGE = _('#results-per-page_select :selected').val();
-			_(document.forms[0]).submit();
+			RESULTS_PER_PAGE = $('#results-per-page_select :selected').val();
+			$(document.forms[0]).submit();
 		//	historyModel.setSearchText(searchBox.val(), 0,options);
 		});
 		chrome.history.onVisitRemoved.addListener(historyDeleted);
 		
 		//add show url checkbox
-		_('#edit-button').append('<div id="div-show-urls"><input type="checkbox" id="show-urls" />Show URLs</div>');
+		$('#edit-button').append('<div id="div-show-urls"><input type="checkbox" id="show-urls" />Show URLs</div>');
 		$('#show-urls').click(function (){
 			var checked = $(this).is(":checked");
-			console.log(checked);
 			$('.title a').each(function () {
-				var href = $(this).attr('href');
+				var $this = $(this);
+				var href = $this.attr('href');
 				if (checked){
-					$(this).data('title', $(this).text());
-					$(this).text('');
-					Page.prototype.addHighlightedText_($(this).get(0), href, historyModel.getSearchText());
+					$this.data('title', $this.text());
+					$this.text('');
+					Page.prototype.addHighlightedText_($this.get(0), href, historyModel.getSearchText());
 				}else{
-					var text = $(this).data('title');
-					$(this).text('');
-					Page.prototype.addHighlightedText_($(this).get(0), text, historyModel.getSearchText());
+					var text = $this.data('title');
+					$this.text('');
+					Page.prototype.addHighlightedText_($this.get(0), text, historyModel.getSearchText());
 				}
 				
 			});
 		});
 	});
 
-	})(jQuery);
+})(jQuery);
