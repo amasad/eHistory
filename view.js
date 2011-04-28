e80cf552417efae794724028524c915d62036209
Amjad Masad <amjad.masad@gmail.com>
2011-04-28 01:14:06 -0700

    Added comments (via github)

--- view.js
+++ view.js
@@ -1,18 +1,29 @@
 (function($){
-/******** VIEW **********/
+/************************ VIEW ************************/
+// History View, responsible for populating results in the current page view
+// Holds current page state, and updates page controls accordingly 
+// Direct communication with history model
 /** global */
 historyView = (function(){
+  // Initial DOM (jQuery) variables 
   var $table,$olderPage, $newerPage, $firstPage, $lastPage, $allNav;
+  // Current page in the history view
   var currentPage = 0;
-  //init
+  //On domReady get the DOM elements
   $(function(){
+    // Main table that holds results
     $table = $('#tbl-main');
+    // Button responsible for getting older results, i.e. previous page
     $olderPage = $('#btn-older');
+    // Button for getting newer results, i.e. next page
     $newerPage = $('#btn-newer');
+    // Button for getting the first page
     $firstPage = $('#btn-first');
+    // Button to get the last page
     $lastPage = $('#btn-last');
+    // jQuery instance holding all the navigation controls
     $allNav = $olderPage.add($newerPage).add($firstPage).add($lastPage);
-
+    // Bind buttons functionalities
     $olderPage.click(function (){
       currentPage++;
       $(historyModel).trigger("modelrefresh");
@@ -25,34 +36,44 @@ historyView = (function(){
       currentPage = 0;
       $(historyModel).trigger("modelrefresh"); 
     });
+    // TODO: Implement
     $lastPage.click(function (){
       
     });
+    // Navigation controls disabled onload
     $allNav.attr("disabled", true);
   });
-
+  // when the view is on the first page disable first, newer buttons
   function updateControls(){
     $allNav.attr("disabled", false);
     if (currentPage === 0){
       $firstPage.add($newerPage).attr("disabled", true);
     }
   }
+  // Listen to history model refresh event
+  // and get the page required from the model
   $(historyModel).bind("modelrefresh", function(e, args){
     updateControls();
     var page = this.getPage(currentPage);
     if (page === -1)  return;
+    // results per day hash
     var results_day = {};
     $.each(page, function (i, visit) {
       if (!results_day[visit.day]) results_day[visit.day] = [];
       results_day[visit.day].push(visit);
     });
+    // empty the view table
     $table.empty();
+    // for each day create a day row (holds day info and a checkbox allows selection of all day items)
     $.each(results_day, function (day, items) {
       $.tmpl('day-row', {date: new Date(parseInt(day)).toDateString()}).appendTo($table);
+      // on each day populate the results that corresponds to that day.
       $.each(items, function(i, visit){
         var row = $.tmpl('row', visit);
+        // let the elem data hold info of the corrosponding visit
         row.data('id', visit.id);
         row.data('day', visit.day);
+        // check if the current item to be populated was selected in earlier navigations
         if (historyModel.isSelected(visit.id, visit.day)) {
           row.children().children("input").attr("checked", true);
         }
@@ -61,11 +82,11 @@ historyView = (function(){
     })
    
   });
-  
+  // listens for lastPage event from the historyModel update controls accordingly 
   $(historyModel).bind("lastPage", function(e, args){
     $olderPage.add($lastPage).attr("disabled", true);
   });
-
+   // Public functions
    return {
     clear: function () {
       currentPage = 0;
