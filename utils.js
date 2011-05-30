1673b2cc96dbab9dbfc4bb64cac3c1b9093726dd
Amjad Masad <amjad.masad@gmail.com>
2011-05-31 02:47:44 +0300

    Cleaned after stats

--- utils.js
+++ utils.js
@@ -1,10 +1,24 @@
 Utils = {
-  parseUrl: function (url){
+  parseUrl: function (url) {
     url = url.replace(/http(s)*:\/\//, "").replace(/:[0-9]+/,'');
     var hostName = url.split('/')[0];
     return {
       hostName: hostName,
       path: url.replace(hostName + "/", "")
     };
+  },
+  daysInMonth: function(mnth, yr) {
+    return 32 - new Date(mnth, yr, 32).getDate();
+  },
+  dayOnYear: function(date) {
+    var day, i, month, ret, yr;
+    day = date.getDate();
+    month = date.getMonth();
+    yr = date.getFullYear();
+    ret = 0;
+    for (i = 0; 0 <= month ? i <= month : i >= month; 0 <= month ? i++ : i--) {
+      ret += Utils.daysInMonth(i, yr);
+    }
+    return ret += day;
   }
 };
