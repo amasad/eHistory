cc659f9197366dcab790c5443e160a575cea4547
Amjad Masad <amjad.masad@gmail.com>
2013-09-09 14:45:30 -0400

    more optimization

--- view.js
+++ view.js
@@ -124,11 +124,11 @@
           corners: 1, // Corner roundness (0..1)
           rotate: 0, // The rotation offset
           direction: 1, // 1: clockwise, -1: counterclockwise
-          color: '#000', // #rgb or #rrggbb or array of colors
+          color: '#333', // #rgb or #rrggbb or array of colors
           speed: 1, // Rounds per second
           trail: 60, // Afterglow percentage
           shadow: false, // Whether to render a shadow
-          hwaccel: false, // Whether to use hardware acceleration
+          hwaccel: true, // Whether to use hardware acceleration
           className: 'spinner', // The CSS class to assign to the spinner
           zIndex: 2e9, // The z-index (defaults to 2000000000)
           top: '0', // Top position relative to parent in px
