65c83626bbad357c002b5c24a77a57486047d7d0
Amjad Masad <amjad.masad@gmail.com>
2013-09-10 23:09:30 -0400

    instantiate spinner once

--- view.js
+++ view.js
@@ -28,6 +28,25 @@
       'day-row': Mustache.compile('<tr class="hdr-day"><td><input type="checkbox" class="chk-day"/></td><td class="date" colspan="2">{{date}}</td></tr>')
     };
 
+    var spinner = new Spinner({
+      lines: 13, // The number of lines to draw
+      length: 10, // The length of each line
+      width: 2, // The line thickness
+      radius: 10, // The radius of the inner circle
+      corners: 1, // Corner roundness (0..1)
+      rotate: 0, // The rotation offset
+      direction: 1, // 1: clockwise, -1: counterclockwise
+      color: '#333', // #rgb or #rrggbb or array of colors
+      speed: 1, // Rounds per second
+      trail: 60, // Afterglow percentage
+      shadow: false, // Whether to render a shadow
+      hwaccel: true, // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9, // The z-index (defaults to 2000000000)
+      top: '0', // Top position relative to parent in px
+      left: 'auto' // Left position relative to parent in px
+    });
+
     //init
     //On domReady get the DOM elements
     $(function(){
@@ -116,24 +135,7 @@
       },
       displayThrobber: function () {
         $throbber.addClass('active');
-        new Spinner({
-          lines: 13, // The number of lines to draw
-          length: 10, // The length of each line
-          width: 2, // The line thickness
-          radius: 10, // The radius of the inner circle
-          corners: 1, // Corner roundness (0..1)
-          rotate: 0, // The rotation offset
-          direction: 1, // 1: clockwise, -1: counterclockwise
-          color: '#333', // #rgb or #rrggbb or array of colors
-          speed: 1, // Rounds per second
-          trail: 60, // Afterglow percentage
-          shadow: false, // Whether to render a shadow
-          hwaccel: true, // Whether to use hardware acceleration
-          className: 'spinner', // The CSS class to assign to the spinner
-          zIndex: 2e9, // The z-index (defaults to 2000000000)
-          top: '0', // Top position relative to parent in px
-          left: 'auto' // Left position relative to parent in px
-        }).spin($throbber[0]);
+        spinner.spin($throbber[0]);
       }
     };
   })();
